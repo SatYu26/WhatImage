ad5eeade9f0d3d585aa29a429fbc54158296ec43
SatYu26 <goyalsatyam8@gmail.com>
2021-03-16 03:20:45 +0530

    adding spinners

--- whatimage-front/src/components/classifier/classifier.js
+++ whatimage-front/src/components/classifier/classifier.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import Dropzone from "react-dropzone";
+import { Spinner } from "react-bootstrap";
 import "./classifier.css";
 
 class Classifier extends Component {
   state = {
     files: [],
+    isLoading: false,
   };
 
   onDrop = (files) => {
-    this.setState({ files });
+    this.setState({ isLoading: true });
+    this.loadImage(files);
+  };
+
+  loadImage = (files) => {
+    setTimeout(() => {
+      this.setState({
+        files,
+        isLoading: false,
+      });
+    }, 1000);
   };
 
   render() {
@@ -19,17 +31,26 @@ class Classifier extends Component {
     ));
 
     return (
-      <Dropzone onDrop={this.onDrop}>
-        {({ getRootProps, getInputProps }) => (
+      <Dropzone onDrop={this.onDrop} accept="image/png, image/jpg">
+        {({ isDragActive, getRootProps, getInputProps }) => (
           <section className="container">
             <div {...getRootProps({ className: "dropzone back" })}>
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>
+                {isDragActive
+                  ? "Drop some images"
+                  : "Drag and drop some files here, or click to select files"}
+              </p>
             </div>
             <aside>
               <h4>Files</h4>
               <ul>{files}</ul>
             </aside>
+            {this.state.isLoading && (
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            )}
           </section>
         )}
       </Dropzone>
