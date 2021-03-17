370dce256cf02b594e68397093b9502b1080d9f3
SatYu26 <goyalsatyam8@gmail.com>
2021-03-18 01:10:39 +0530

    Adding load more images button

--- whatimage-front/src/components/ImageList/ImageList.js
+++ whatimage-front/src/components/ImageList/ImageList.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Image from "./image.js";
+import { Button } from "react-bootstrap";
 
 class ImageList extends Component {
   state = {
     images: [],
+    visible: 2,
   };
 
   componentDidMount() {
@@ -21,14 +23,26 @@ class ImageList extends Component {
         console.log(resp);
       });
   };
+
+  handleVisible = () => {
+    const visible = this.state.visible;
+    const new_visible = visible + 2;
+    this.setState({
+      visible: new_visible,
+    });
+  };
+
   render() {
-    const images = this.state.images.map((img) => {
+    const images = this.state.images.slice(0, this.state.visible).map((img) => {
       return <Image key={img.id} picture={img.picture} name={img.classified} />;
     });
     return (
       <div>
-        <h1>ImageList here</h1>
+        <h1>Image List Here</h1>
         {images}
+        <Button variant="primary" size="lg" onClick={this.handleVisible}>
+          Load more images
+        </Button>
       </div>
     );
   }
--- whatimage-front/src/components/ImageList/image.js
+++ whatimage-front/src/components/ImageList/image.js
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 
 const Image = (props) => {
   return (
-    <Card style={{ width: "18rem" }} className="mx-auto mb-2">
+    <Card style={{ width: "30rem" }} className="mx-auto mb-2">
       <Card.Img variant="top" src={props.picture} />
       <Card.Body>
         <Card.Title>Classified as: {props.name}</Card.Title>
